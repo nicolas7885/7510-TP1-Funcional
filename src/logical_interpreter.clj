407e08ed09169e0437aefaebcd3b9ffb4add981d
nicolas7885 <nicolas7885@hotmail.com>
2017-09-03 18:40:55 -0300

    trouble running

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -4,4 +4,22 @@
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  nil)
+  nil
+)
+
+(defn validate-line
+	"returns true if it is valid, false if not"
+	[line]
+	(let[no-dot-line (replace line ".clojure.string/split-lines" "")
+		name-and-params-separated (clojure.string/split line "(" )]
+		nil
+	)
+)
+
+(defn validate-database
+	"returns true if it is valid, false if not"
+	[db]
+	(let[lines (clojure.string/split-lines db)]
+	(every? validate-line lines) 
+	)
+)
